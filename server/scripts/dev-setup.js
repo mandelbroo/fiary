7e473cfa53edcce5e6c8202c36224ad745dc89e6
vitaliy <vitaliy@globalit.no>
2017-10-10 16:24:55 +0300

    turn on api logging

--- server/scripts/dev-setup.js
+++ server/scripts/dev-setup.js
@@ -1,8 +1,9 @@
 const connection = require('../../db/connection')
 const logger = require('morgan-body')
 
+logger(app)
+
 module.exports = (app) => {
-  logger(app)
   connection
     .authenticate()
     .then(() => console.log('DB connection: OK'))
