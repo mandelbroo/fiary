e0e4ccb6b94f4bee22d76c3e5d262574d019bb50
vitaliy <vitaliy@globalit.no>
2017-10-10 16:48:12 +0300

    heroku does not support that

--- server/scripts/dev-setup.js
+++ server/scripts/dev-setup.js
@@ -1,9 +1,8 @@
 const connection = require('../../db/connection')
 const logger = require('morgan-body')
 
-logger(app)
-
 module.exports = (app) => {
+  logger(app)
   connection
     .authenticate()
     .then(() => console.log('DB connection: OK'))
