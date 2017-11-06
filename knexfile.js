47e894840d0b9b20605331f1c04839da8dd1ae18
vitalii <vitalii.makarets@gmail.com>
2017-11-06 13:33:25 +0200

    prod setup migrations

--- knexfile.js
+++ knexfile.js
@@ -39,14 +39,15 @@ module.exports = {
     connection: process.env.DATABASE_URL || {
       database : process.env.DB_NAME || 'fiary_prod',
       host : process.env.DB_HOST || '127.0.0.1',
-      password : process.env.DB_PASS,
-      user : process.env.DB_USER,
+      password : process.env.DB_PASS || 'verysecret',
+      user : process.env.DB_USER || 'postgres',
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
+      directory: './db/migrations',
       tableName: 'knex_migrations'
     }
   }
