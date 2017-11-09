20198b6ba352c98e15362b3551bc29f6e924873a
vitalii <vitalii.makarets@gmail.com>
2017-11-09 19:18:27 +0200

    better skip than comment out

--- server/tests/routes/status-codes.test.js
+++ server/tests/routes/status-codes.test.js
@@ -2,22 +2,22 @@ const app = require('../../index')
 const request = require('supertest')(app)
 
 describe('status-codes', () => {
-  ////  not sure why this fails on Travic CI (falls into 404 handler instead of sending index.html)
+  ////  Skipped. Not sure why this fails on Travic CI (falls into 404 handler instead of sending index.html)
   ////  https://s3.amazonaws.com/archive.travis-ci.org/jobs/298475545/log.txt?X-Amz-Expires=30&X-Amz-Date=20171109T171403Z&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJRYRXRSVGNKPKO5A/20171109/us-east-1/s3/aws4_request&X-Amz-SignedHeaders=host&X-Amz-Signature=4df6e5069299df192ff23918be8906e8b16478f5336b11e24df9622b4ccb4a5e
-  // describe('200', () => {
-  //   it("return index.html on get not existing route", done => {
-  //     request
-  //       .get('/not-exist')
-  //       .type('json')
-  //       .send({})
-  //       .expect(200)
-  //       .expect('Content-Type', 'text/html; charset=UTF-8')
-  //       .end((err, res) => {
-  //         expect(res).toBeDefined()
-  //         done(err)
-  //       })
-  //   })
-  // })
+  describe.skip('200', () => {
+    it("return index.html on get not existing route", done => {
+      request
+        .get('/not-exist')
+        .type('json')
+        .send({})
+        .expect(200)
+        .expect('Content-Type', 'text/html; charset=UTF-8')
+        .end((err, res) => {
+          expect(res).toBeDefined()
+          done(err)
+        })
+    })
+  })
   describe('400', () => {
     it('fire when validation fails', done => {
       request
