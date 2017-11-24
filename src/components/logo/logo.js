434ed54fa8f68f0384ca79d1a2d5c06afd1c0a1e
vitalii <vitalii.makarets@gmail.com>
2017-11-24 12:55:08 +0200

    organize logo comp

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import logo from '../../logo.svg'
+import logo from './logo.svg'
 import './logo.css'
 
 export default class Logo extends React.Component {
   render() { return (
-      <Link to='/'>
+      <a href='/'>
         <img src={logo} className="logo" alt="logo" />
-      </Link>
+      </a>
   )}
 }
