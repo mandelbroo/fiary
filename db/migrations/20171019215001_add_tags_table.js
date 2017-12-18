68a345ae5398c0dbd377dcafc52e4420ce83b7f6
vitalii <vitalii.makarets@gmail.com>
2017-12-18 14:04:59 +0200

    unique tag names

--- db/migrations/20171019215001_add_tags_table.js
+++ db/migrations/20171019215001_add_tags_table.js
@@ -1,7 +1,7 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('tags', (table) => {
     table.increments('id').primary().notNullable()
-    table.string('name').notNullable()
+    table.string('name').notNullable().unique('name')
     table.timestamps()
   })
 }
