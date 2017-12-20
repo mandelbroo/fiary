e00e0f8366cd4f859275dc9803556f14d7b7a8b2
vitalii <vitalii.makarets@gmail.com>
2017-12-20 17:01:45 +0200

    tagger component

---
+++ src/components/entries/tagger.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+
+export default class Tagger extends Component {
+  state = {
+    tags: this.props.tags || [],
+    currentValue: false
+  }
+
+  click = () => {
+    this.setState({tags: this.state.tags.concat([this.state.currentValue])})
+  }
+
+  change = (event) => {
+    this.setState({currentValue: event.target.value})
+  }
+
+  render() {
+    return (
+      <div>
+        <input type='text' onChange={this.change}/>
+        <button onClick={this.click}>Add</button>
+      </div>
+    )
+  }
+}
---
+++ src/components/entries/tagger.test.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../../config/enzyme'
+import { shallow } from 'enzyme'
+import Tagger from './tagger'
+
+describe('Tagger', () => {
+  it('initial render', () => {shallow(<Tagger />)})
+  it('contains input and button', () => {
+    const wrapper = shallow(<Tagger />)
+    expect(wrapper.containsAllMatchingElements([
+      <input type='text' />,
+      <button>Add</button>
+    ])).toBe(true)
+  })
+  it('set tags with prop', () => {
+    const tags = ['one', 'two', 'three']
+    const wrapper = shallow(<Tagger tags={tags}/>)
+    expect(wrapper.state('tags')).toMatchObject(tags)
+  })
+  it('add a tag', () => {
+    const wrapper = shallow(<Tagger />)
+    wrapper.find('input').simulate('change', {target: {value: 'new-tag'}})
+    wrapper.find('button').simulate('click')
+    expect(wrapper.state('tags')).toMatchObject(['new-tag'])
+  })
+  it('shows added tags')
+  it('delete tag')
+})
