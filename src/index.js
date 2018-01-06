63d5c70e29e5b36a6b230d295b7cfae856ce5c96
vitalii <vitalii.makarets@gmail.com>
2018-01-06 18:27:51 +0200

    appreciate warnings

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import { BrowserRouter, Route } from 'react-router-dom'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+//import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render((
   <BrowserRouter>
