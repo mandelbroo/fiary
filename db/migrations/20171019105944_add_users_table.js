b0188b8531d9a1e7dfe007723655ffd5f1cfdafb
vitalii <vitalii.makarets@gmail.com>
2018-01-11 17:58:22 +0200

    username and email uniqueness

--- db/migrations/20171019105944_add_users_table.js
+++ db/migrations/20171019105944_add_users_table.js
@@ -1,10 +1,11 @@
 exports.up = (knex, Promise) => {
   return knex.schema.createTable('users', (table) => {
     table.increments('id').primary().notNullable()
-    table.string('username').notNullable()
+    table.string('username').notNullable().unique('username')
     table.string('password').notNullable()
-    table.string('email').notNullable()
+    table.string('email').notNullable().unique('email')
     table.string('role').notNullable().defaultTo('user')
+    table.unique(['username', 'email'])
     table.timestamps()
   })
 }
