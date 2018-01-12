97f09597a2c0013ccb853c68b54a2140315c8015
vitalii <vitalii.makarets@gmail.com>
2018-01-12 15:52:54 +0200

    signin comp handle errors and 100% test cov

--- src/components/signin/signin.js
+++ src/components/signin/signin.js
@@ -7,39 +7,63 @@ export default class Signin extends React.Component {
     password: ''
   }
 
-  handleChange = ({target}) => {
+  user = this.props.user || User  // DI for tests
+
+  change = ({target}) => {
     let newState = {}
     newState[target.name] = target.value
     this.setState(newState)
+    this._errorMessage = ''
   }
 
-  handleSubmit = async (event) => {
+  submit = async (event) => {
     event.preventDefault()
-    const res = await User.signin(this.state)
-    const nextRoute = res.success ? '/' : '/signin'
-    this.props.history.push(nextRoute)
+    const res = await this.user.signin(this.state)
+    if (res.success)
+      this.props.history.push('/')
+    else
+      this.props.history.push('/signin', {error: res.message})
+  }
+
+  get locationState () {
+    return this.props.location && this.props.location.state
+      ? this.props.location.state : {}
+  }
+
+  set locationState (value) { this.props.location.state = value }
+
+  get errorMessage() {
+    if (this.locationState.error) {
+      this._errorMessage = this.locationState.error
+      this.locationState = {}
+    }
+    return this._errorMessage
   }
 
   render() {
     return (
       <div>
         <h3>Enter</h3>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.submit}>
           <div>
             <input className="text-field" type="email" name="email"
-              onChange={this.handleChange}
+              onChange={this.change}
               placeholder='Email'
               autoComplete='email'
               required />
           </div>
           <div>
             <input className="text-field" type="password" name="password"
-              onChange={this.handleChange}
+              onChange={this.change}
               placeholder='Password'
               autoComplete='current-password'
               required />
           </div>
           <input className="button-primary" type="submit" />
+          {this.errorMessage
+            ? <div name='error' style={{color: 'red'}}>{this.errorMessage}</div>
+            : null
+          }
         </form>
       </div>
     )
