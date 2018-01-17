a690c6a6852b45b367e5ea32a915393e01003b2e
vitalii <vitalii.makarets@gmail.com>
2018-01-17 14:07:14 +0200

    fix record-list test

--- src/components/record-list/record-list.test.js
+++ src/components/record-list/record-list.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../../config/enzyme'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import RecordList from './record-list'
 
 describe('RecordList', () => {
@@ -9,14 +9,14 @@ describe('RecordList', () => {
     expect(wrapper.children().length).toBe(0)
   })
   it('data prop set state', () => {
-    const list = [
+    const data = [
       {amount: 80, tags:['lunch']},
       {amount: 10, tags:['tips']},
     ]
-    const wrap = shallow(<RecordList data={list}/>)
+    const wrap = mount(<RecordList data={data}/>)
     const recordViews = wrap.find('RecordView')
     expect(recordViews.length).toBe(2)
-    expect(recordViews.first().props().data).toMatchObject(list[0])
-    expect(recordViews.last().props().data).toMatchObject(list[1])
+    expect(recordViews.first().props().data).toMatchObject(data[0])
+    expect(recordViews.last().props().data).toMatchObject(data[1])
   })
 })
