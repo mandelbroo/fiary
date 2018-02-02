b08fef23a260e22d4ac65291563d731844c9b9c8
vitalii <vitalii.makarets@gmail.com>
2018-02-02 12:04:02 +0200

    error case tests

--- server/tests/routes/records.test.js
+++ server/tests/routes/records.test.js
@@ -2,6 +2,7 @@ const server = require('../../app').listen()
 const app = require('supertest').agent(server)
 const User = require('../../models/user')
 const Entry = require('../../models/entry')
+const Tag = require('../../models/tag')
 const jwtGenerate = require('../../utils/jwt-generate')
 const {DateTime} = require('luxon')
 
@@ -37,6 +38,23 @@ describe('records route', () => {
           .expect(200)
           .end((err, res) => {
             expect(res.body.success).toBeTruthy()
+            done(err)
+          })
+    })
+    it('handle error', async(done) => {
+      await Tag.findOrCreate({name: 'exist'})
+      app.post('/api/records')
+        .send({
+          entryId: entry.id,
+          amount: 20,
+          income: false,
+          tags: [{ id: -1, name: 'exist' }]
+        })
+        .set('Authorization', token)
+          .expect(422)
+          .end((err, res) => {
+            expect(res.body.success).toBe(false)
+            expect(res.body.message).toBeDefined()
             done(err)
           })
     })
