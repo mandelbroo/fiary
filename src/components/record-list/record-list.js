dae79f3f4dbc60f59f0bed417de11599a7d31ae0
vitalii <vitalii.makarets@gmail.com>
2018-02-06 14:26:13 +0200

    remove button in record list

--- src/components/record-list/record-list.js
+++ src/components/record-list/record-list.js
@@ -11,22 +11,40 @@ const style = {
     color: 'lightgrey'
   },
   listItem: {
-    padding: '5px'
-  }
+    padding: '5px',
+    '& div': {
+      display: 'inline-block'
+    },
+    '& span': {
+      display: 'inline-block'
+    }
+  },
 }
 
 const NoRecords = (className) => <span className={className}>no records yet</span>
 
+export class RemoveButton extends React.Component {
+  render = () => (
+    <span role='img'
+      aria-label='delete record'
+      onClick={this.props.onClick}>❌</span>
+    )
+}
+
 export class RecordList extends React.Component {
   classes = this.props.classes || {}
 
+  removeButton = (item) => this.props.onRemove
+      ? <RemoveButton onClick={ () => this.props.onRemove(item) }/> : ''
+
   render() {
     const items = this.props.data ? [].concat(this.props.data) : []
-    const records = items.map((item, index) => {
-      return (<li key={index} className={this.classes.listItem}>
+    const records = items.map((item, index) => (
+      <li key={index} className={this.classes.listItem}>
         <RecordView data={item} />
+        { this.removeButton(item) }
       </li>)
-    })
+    )
     const content = records.length > 0 ? records : NoRecords(this.classes.noRecords)
     return (<ul className={ this.classes.list }>
         { content }
--- src/components/record-list/record-list.test.js
+++ src/components/record-list/record-list.test.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import '../../config/enzyme'
 import { shallow, mount } from 'enzyme'
-import RecordList from './record-list'
+import RecordList, { NoRecords, RemoveButton } from './record-list'
+
+const data = [
+  {amount: 80, tags:['lunch']},
+  {amount: 10, tags:['tips']},
+]
 
 describe('RecordList', () => {
   it('render initial state', () => {
@@ -9,10 +14,6 @@ describe('RecordList', () => {
     expect(wrapper.children().length).toBe(0)
   })
   it('data prop set state', () => {
-    const data = [
-      {amount: 80, tags:['lunch']},
-      {amount: 10, tags:['tips']},
-    ]
     const wrap = mount(<RecordList data={data}/>)
     const recordViews = wrap.find('RecordView')
     expect(recordViews.length).toBe(2)
@@ -22,5 +23,14 @@ describe('RecordList', () => {
   it('show "no records" text', () => {
     const wrap = mount(<RecordList data={[]}/>)
     expect(wrap.find('span').text()).toBe('no records yet')
+  })
+  it('remove item', () => {
+    const fakeRemoveHandler = jest.fn()
+    const wrapper = mount(
+      <RecordList onRemove={fakeRemoveHandler} data={data}/>)
+    const removeButtons = wrapper.find(RemoveButton)
+    expect(removeButtons.length).toBe(2)
+    removeButtons.first().simulate('click')
+    expect(fakeRemoveHandler).toBeCalledWith(data[0])
   })
 })
