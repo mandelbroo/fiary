eb8fd2a0f0e07f207de3cdb8e67e70e477b2f449
vitalii <vitalii.makarets@gmail.com>
2018-02-16 11:22:51 +0200

    refactor register sw test

--- src/registerServiceWorker.test.js
+++ src/registerServiceWorker.test.js
@@ -1,21 +1,32 @@
 import registerServiceWorker from './registerServiceWorker'
 
+const originalFetch = window.fetch
+const originalServiceWorker = navigator.serviceWorker
+const originalLocationReload = window.location.reload
+
 let registerPromise = { }
 let unregisterPromise = { }
+
 const fakeServiceWorker = {
   register: jest.fn().mockImplementation(() => registerPromise = Promise.resolve({ })),
   ready: Promise.resolve({
     unregister: jest.fn().mockImplementation(() => unregisterPromise = Promise.resolve({ }))
   }),
 }
-navigator.serviceWorker = fakeServiceWorker
-window.location.reload = jest.fn()
-
 
 describe('register service worker', () => {
-  it('successfully', async () => {
+  beforeEach(() => {
     process.env.NODE_ENV = 'production'
+  })
 
+  afterEach(() => {
+    navigator.serviceWorker = originalServiceWorker
+    window.location.reload = originalLocationReload
+    window.fetch = originalFetch
+    process.env.NODE_ENV = 'test'
+  })
+
+  it('successfully', async () => {
     const fakeFetch = jest.fn()
       .mockImplementation(() => Promise.resolve({
         status: 200,
@@ -23,14 +34,14 @@ describe('register service worker', () => {
           get: jest.fn().mockReturnValue('javascript')
         }
       }))
+    navigator.serviceWorker = fakeServiceWorker
     window.fetch = fakeFetch
+
     registerServiceWorker()
     window.dispatchEvent(new Event('load'))
     expect(fakeFetch).toBeCalled()
     await registerPromise
     expect(fakeServiceWorker.register).toBeCalledWith('/service-worker.js')
-
-    process.env.NODE_ENV = 'test'
   })
   it('not found', async () => {
     process.env.NODE_ENV = 'production'
@@ -38,6 +49,9 @@ describe('register service worker', () => {
     const fakeFetch = jest.fn()
       .mockImplementation(() => Promise.resolve({ status: 404 }))
     window.fetch = fakeFetch
+    navigator.serviceWorker = fakeServiceWorker
+    window.location.reload = jest.fn()
+
     registerServiceWorker()
     window.dispatchEvent(new Event('load'))
     expect(fakeFetch).toBeCalled()
@@ -46,7 +60,5 @@ describe('register service worker', () => {
     expect(res.unregister).toBeCalled()
     await unregisterPromise
     expect(window.location.reload).toBeCalled()
-
-    process.env.NODE_ENV = 'test'
   })
 })
