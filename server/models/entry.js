dbdbf345bad28ce6e0a0e5acacb1df26be2d2775
vitalii <vitalii.makarets@gmail.com>
2018-02-23 21:09:04 +0200

    add entry day getter

--- server/models/entry.js
+++ server/models/entry.js
@@ -1,8 +1,13 @@
-const { createModel } = require('./base')
+const { createModel, bookshelf } = require('./base')
 
 const Entry = createModel('Entry', {
 	tableName: 'entries',
-	records: function () { return this.hasMany('Record') }
+	records: function () { return this.hasMany('Record') },
+	constructor: function() {
+		bookshelf.Model.apply(this, arguments)
+		Object.assign(this, this.attributes)
+		this.getter('day', 'day')
+	}
 })
 
 module.exports = Entry
