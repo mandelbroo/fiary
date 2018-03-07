0489f816c67388f0306294d039ce9247f32c6b06
vitalii <vitalii.makarets@gmail.com>
2018-03-07 10:06:45 +0200

    fix: better naming

--- src/store.js
+++ src/store.js
@@ -2,9 +2,8 @@ import { applyMiddleware, createStore } from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import promise from 'redux-promise-middleware'
-
-import reducers from './reducers'
+import rootReducer from './reducers'
 
 const middleware = applyMiddleware(promise(), thunk, logger)
 
-export default createStore(reducers, middleware)
+export default createStore(rootReducer, middleware)
