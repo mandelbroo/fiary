0aa845dfe9a337164e91bf166903ec6fdddf07f0
vmakarets <vitalii.makarets@gmail.com>
2018-07-02 16:09:28 +0300

    fix entries component test

--- src/components/entries/entries.test.js
+++ src/components/entries/entries.test.js
@@ -1,41 +1,36 @@
 import React from 'react'
 import '../../config/enzyme'
 import { shallow, mount } from 'enzyme'
-import Entries from './entries'
+import { Entries } from './entries'
 import DayTile from '../day-tile/day-tile'
 
+jest.mock('../../actions', () => ({ getEntries: jest.fn() }))
+import actions from '../../actions'
+
 describe('Entries component', () => {
-	const fakeRes = {data: {collection: [
-		{id: 24, day: '2016-01-12', records: [{amount: 10, tags: ['a']}]},
-		{id: 25, day: '2016-01-13', records: [{amount: 20, tags: ['b']}]},
-	]}}
 
-	let promise = {}
+	const fakeDispatch = jest.fn()
+	const fakeEntry = { day: '2022-04-20' }
 
-	const fakeEntry = {
-		getAll: jest.fn().mockImplementation(() =>
-			promise = Promise.resolve(fakeRes))
-	}
-
-	it('get entries and render them', async () => {
-		const wrapper = mount(<Entries entry={fakeEntry} />)
-		await promise
-		wrapper.update()
-		expect(wrapper.state('entries')).toBe(fakeRes.data.collection)
-		expect(wrapper.find(DayTile).length).toBe(2)
-		expect(wrapper.find('h4').length).toBe(2)
+	it('get entries dispatch', async () => {
+		const wrapper = mount(<Entries entries={[fakeEntry]} dispatch={fakeDispatch} />)
+		expect(fakeDispatch).toBeCalled()
+		expect(actions.getEntries).toBeCalled()
 	})
+
 	it('clicking on day entry redirects to its page', async () => {
-		const fakeRedirect = jest.fn().mockReturnValue('redirect...')
-		const fakeHistory = {
-			push: jest.fn()
-		}
+		const fakeRedirect = jest.fn().mockReturnValue('')
+		const fakeHistory = { push: jest.fn() }
 		const wrapper = mount(
-			<Entries entry={fakeEntry} redirect={fakeRedirect} history={fakeHistory}/>)
-		await promise
-		wrapper.update()
+			<Entries
+				entries={[fakeEntry]}
+				dispatch={fakeDispatch}
+				redirect={fakeRedirect}
+				history={fakeHistory} />
+		)
 		wrapper.find(DayTile).first().simulate('click')
-		expect(fakeRedirect).toBeCalledWith('/entry/24')
-		expect(fakeHistory.push).toBeCalledWith('/entry/24')
+		const expected = `/entry/${fakeEntry.day}`
+		expect(fakeRedirect).toBeCalledWith(expected)
+		expect(fakeHistory.push).toBeCalledWith(expected)
 	})
 })
