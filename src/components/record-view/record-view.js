7915c00940056897c1cdfd0c003afae04b02140b
vmakarets <vitalii.makarets@gmail.com>
2018-07-12 21:28:00 +0300

    fixes record-view

--- src/components/record-view/record-view.js
+++ src/components/record-view/record-view.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import injectSheet from 'react-jss'
+import PropTypes from 'prop-types';
 
-import style from './style'
+import styles from './styles'
 
 export class RecordView extends React.Component {
 	TagView = (tag, index) =>
-		<span className={ this.props.classes.tag }key={ index }>{ tag.name }</span>
+		<span className={this.props.classes.tag} key={index}>{tag.name}</span>
 
 	render() {
-		const data = this.props.data
-		const operation = data.income ? '+' : '-'
-		const operClass = data.income ? this.props.classes.plus : this.props.classes.minus
+		const { data, classes } = this.props
+		const operClass = data.income ? classes.plus : classes.minus
 		return (
-			<div>
-				<span className={ operClass }>{ operation }</span>
-				<span className={ operClass }>{ data.amount }</span>
+			<div className={classes.container}>
+				<span className={operClass}>{ data.amount }</span>
 				{ data.tags.map(this.TagView) }
 			</div>)
 	}
 }
+RecordView.propTypes = {
+	classes: PropTypes.object.isRequired,
+	data: PropTypes.object.isRequired,
+  editMode: PropTypes.bool,
+}
 
-export default injectSheet(style)(RecordView)
+export default injectSheet(styles)(RecordView)
