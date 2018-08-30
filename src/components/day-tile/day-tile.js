f6ba05b4b8c077d6a8db012d99c5cc43f66c7f55
Vitalii <vitalii.makarets@gmail.com>
2018-08-30 11:03:55 +0300

    fix day-tile test

--- src/components/day-tile/day-tile.js
+++ src/components/day-tile/day-tile.js
@@ -1,22 +1,28 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import RecordList from '../record-list/record-list'
 
 import injectSheet from 'react-jss'
 import styles from './styles.js'
 
 export class DayTile extends React.Component {
-	entry = this.props.entry || {}
-	classes = this.props.classes || {}
+  entry = this.props.entry || {}
+  classes = this.props.classes || {}
 
-	click = () => this.props.click(this.entry.day)
+  click = () => this.props.click(this.entry.day)
 
-	render() {
-		return (
-			<div className={ this.classes.tile } onClick={this.click}>
-				<h4>{ this.entry.day }</h4>
-				<RecordList data={this.entry.records} />
-			</div>)
-	}
+  render() {
+    return (
+      <div className={this.classes.tile} onClick={this.click}>
+        <h4>{this.entry.day}</h4>
+        <RecordList data={this.entry.records} />
+      </div>
+    )
+  }
+}
+
+DayTile.propTypes = {
+  click: PropTypes.func.isRequired,
 }
 
 export default injectSheet(styles)(DayTile)
--- src/components/day-tile/day-tile.test.js
+++ src/components/day-tile/day-tile.test.js
@@ -4,16 +4,16 @@ import { shallow, mount } from 'enzyme'
 import { DayTile } from './day-tile'
 
 describe('DayTile', () => {
-	it('success mount', () => {
-		const wrapper = mount(<DayTile />)
-	})
-	it('call provided click handler', () => {
-		const fakeClick = jest.fn()
-		const fakeEntry = {
-			id: 777
-		}
-		const wrapper = mount(<DayTile click={fakeClick} entry={fakeEntry}/>)
-		wrapper.simulate('click')
-		expect(fakeClick).toBeCalledWith(777)
-	})
+  it('success mount', () => {
+    const wrapper = mount(<DayTile click={() => {}} />)
+  })
+  it('call provided click handler', () => {
+    const fakeClick = jest.fn()
+    const fakeEntry = {
+      day: '2018-01-21',
+    }
+    const wrapper = mount(<DayTile click={fakeClick} entry={fakeEntry} />)
+    wrapper.simulate('click')
+    expect(fakeClick).toBeCalledWith(fakeEntry.day)
+  })
 })
