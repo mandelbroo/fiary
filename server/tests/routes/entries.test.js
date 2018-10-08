a698734d2d9ddb44fc2ce2afd17eb8bface620ac
Vitalii <vitalii.makarets@gmail.com>
2018-10-08 13:40:10 +0300

    try to fix weird babel error

--- server/tests/routes/entries.test.js
+++ server/tests/routes/entries.test.js
@@ -3,115 +3,119 @@ const Entry = require('../../models/entry')
 const User = require('../../models/user')
 const server = require('../../app').listen()
 const app = require('supertest').agent(server)
-const {DateTime} = require('luxon')
+const { DateTime } = require('luxon')
 
 describe('entries', () => {
-	afterAll(async () => {
-		await Promise.all([
-			Entry.connection.destroy(),
-			User.connection.destroy(),
-		])
-		server.close()
-	})
+  let citizen
+  let token
+  let entries
 
-	beforeAll(async () => {
-		citizen = await User.create({
-			username: `${Date.now()}admin`,
-			email: `${Date.now()}admin@email.net`,
-			password: 'Supersecret098'
-		})
-		token = jwtGenerate(citizen.attributes)
-		entries = await Promise.all([
-			Entry.create({
-				userId: citizen.id,
-				day: DateTime.local().toISODate()
-			}),
-			Entry.findOrCreate({
-				userId: citizen.id,
-				day: '2017-05-15'
-			}),
-		])
-	})
+  afterAll(async () => {
+    await Promise.all([Entry.connection.destroy(), User.connection.destroy()])
+    server.close()
+  })
 
-	describe('get /api/entries', () => {
-		it('success case', done => {
-			app.get('/api/entries')
-			.set('Authorization', token)
-			.expect(200)
-			.end((err, { body }) => {
-				expect(body).toBeDefined()
-				expect(body.length).toBe(2)
-				expect(body.totalCount).toBe(2)
-				expect(body.collection).toBeDefined()
-				expect(body.collection.length).toEqual(2)
-				done(err)
-			})
-		})
+  beforeAll(async () => {
+    citizen = await User.create({
+      username: `${Date.now()}admin`,
+      email: `${Date.now()}admin@email.net`,
+      password: 'Supersecret098',
+    })
+    token = jwtGenerate(citizen.attributes)
+    entries = await Promise.all([
+      Entry.create({
+        userId: citizen.id,
+        day: DateTime.local().toISODate(),
+      }),
+      Entry.findOrCreate({
+        userId: citizen.id,
+        day: '2017-05-15',
+      }),
+    ])
+  })
 
-		it('order by day desc', async() => {
-			const firstDay = '2012-01-01';
-			const secondDay = '2010-02-02';
-			const scenario = await Promise.all([
-				Entry.create({
-					userId: citizen.id,
-					day: firstDay
-				}),
-				Entry.create({
-					userId: citizen.id,
-					day: secondDay
-				}),
-			])
-			app.get('/api/entries')
-			.set('Authorization', token)
-			.expect(200)
-			.end(async (err, { body }) => {
-				expect(body.length).toBe(4)
-				const indexes = await Promise.all([
-					body.collection.findIndex(el => el.day === firstDay),
-					body.collection.findIndex(el => el.day === secondDay),
-				])
-				expect(indexes[0] < indexes[1]).toBe(true)
-				await Promise.all(
-					scenario.map(entry => entry.destroy())
-				)
-			})
-		})
-	})
+  describe('get /api/entries', () => {
+    it('success case', (done) => {
+      app
+        .get('/api/entries')
+        .set('Authorization', token)
+        .expect(200)
+        .end((err, { body }) => {
+          expect(body).toBeDefined()
+          expect(body).toHaveLength(2)
+          expect(body.totalCount).toBe(2)
+          expect(body.collection).toBeDefined()
+          expect(body.collection).toHaveLength(2)
+          done(err)
+        })
+    })
 
-	it('get /api/entries/:id', done => {
-		const entry = entries[0]
-		app.get('/api/entries/' + entry.id)
-			.set('Authorization', token)
-			.expect(200)
-			.end((err, { body }) => {
-				expect(body.id).toBe(entry.id)
-				expect(body.userId).toBe(entry.userId)
-				expect(body.day).toBe(entry.day)
-				done(err)
-			})
-	})
-	describe('get /api/entries/:isoDate', () => {
-		it('find entry', done => {
-			app.get('/api/entries/2017-05-15')
-				.set('Authorization', token)
-				.expect(200)
-				.end((err, { body }) => {
-					expect(body).toBeDefined()
-					expect(body.id).toBeDefined()
-					expect(body.userId).toBeDefined()
-					expect(body.day).toBeDefined()
-					done(err)
-				})
-			})
-		it('entry not exists', done => {
-			app.get('/api/entries/2015-01-02')
-				.set('Authorization', token)
-				.expect(204)
-				.end((err, { body }) => {
-					expect(body).toBeDefined()
-					expect(body).toMatchObject({})
-					done(err)
-				})
-			})
-	})
+    it('order by day desc', async () => {
+      const firstDay = '2012-01-01'
+      const secondDay = '2010-02-02'
+      const scenario = await Promise.all([
+        Entry.create({
+          userId: citizen.id,
+          day: firstDay,
+        }),
+        Entry.create({
+          userId: citizen.id,
+          day: secondDay,
+        }),
+      ])
+      app
+        .get('/api/entries')
+        .set('Authorization', token)
+        .expect(200)
+        .end(async (err, { body }) => {
+          expect(body).toHaveLength(4)
+          const indexes = await Promise.all([
+            body.collection.findIndex((el) => el.day === firstDay),
+            body.collection.findIndex((el) => el.day === secondDay),
+          ])
+          expect(indexes[0] < indexes[1]).toBe(true)
+          await Promise.all(scenario.map((entry) => entry.destroy()))
+        })
+    })
+  })
+
+  it('get /api/entries/:id', (done) => {
+    const entry = entries[0]
+    app
+      .get('/api/entries/' + entry.id)
+      .set('Authorization', token)
+      .expect(200)
+      .end((err, { body }) => {
+        expect(body.id).toBe(entry.id)
+        expect(body.userId).toBe(entry.userId)
+        expect(body.day).toBe(entry.day)
+        done(err)
+      })
+  })
+  describe('get /api/entries/:isoDate', () => {
+    it('find entry', (done) => {
+      app
+        .get('/api/entries/2017-05-15')
+        .set('Authorization', token)
+        .expect(200)
+        .end((err, { body }) => {
+          expect(body).toBeDefined()
+          expect(body.id).toBeDefined()
+          expect(body.userId).toBeDefined()
+          expect(body.day).toBeDefined()
+          done(err)
+        })
+    })
+    it('entry not exists', (done) => {
+      app
+        .get('/api/entries/2015-01-02')
+        .set('Authorization', token)
+        .expect(204)
+        .end((err, { body }) => {
+          expect(body).toBeDefined()
+          expect(body).toMatchObject({})
+          done(err)
+        })
+    })
+  })
 })
