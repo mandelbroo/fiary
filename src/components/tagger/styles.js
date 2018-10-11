45c5af77b4d2f30a69614666622cd17368299ba1
Vitalii <vitalii.makarets@gmail.com>
2018-10-11 11:28:33 +0300

    fix tagger

--- src/components/tagger/styles.js
+++ src/components/tagger/styles.js
@@ -1,17 +1,22 @@
-export default{
-	main: {
-		'& input': {
-			fontSize: 20,
-			borderStyle: 'none',
-			'&:focus': {
-				outline: 'none'
-			}
-		},
-		'& .tag': {
-			borderStyle: 'solid',
-	    borderWidth: '1px',
-	    borderRadius: '3px',
-	    padding: '1px 4px'
-		}
-	}
+export default {
+  main: {
+    '& input': {
+      fontSize: 20,
+      borderStyle: 'none',
+      '&:focus': {
+        outline: 'none',
+      },
+    },
+    '& .tag': {
+      borderStyle: 'solid',
+      borderWidth: '1px',
+      borderRadius: '3px',
+      display: 'inline-block',
+      cursor: 'pointer',
+      padding: '1px 4px',
+      '&:not(:first-child)': {
+        marginRight: '10px',
+      },
+    },
+  },
 }
--- src/components/tagger/tagger.js
+++ src/components/tagger/tagger.js
@@ -1,100 +1,119 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import propTypes from 'prop-types'
 
-import {jss} from 'react-jss'
+import { jss } from 'react-jss'
 import styles from './styles'
 
-export default class Tagger extends Component {
-	state = {
-		tags: this.props.tags || [],
-		currentValue: '',
-		suggestions: []
-	}
-	classes = jss.createStyleSheet(styles).attach().classes
+class Tagger extends Component {
+  state = {
+    tags: this.props.tags || [],
+    currentValue: '',
+    suggestions: [],
+  }
+  classes = jss.createStyleSheet(styles).attach().classes
 
-	add = (event) => {
-		if (event.key === 'Enter') {
-			event.preventDefault()
-			const val = this.state.currentValue
-			if (val && val.length > 0) {
-				const newTag = {
-					id: - (this.state.tags.length + 1),
-					name: val
-				}
-				const newTags = this.state.tags.concat([newTag])
-				this.setState({
-					tags: newTags,
-					currentValue: ''
-				})
-				if (this.props.onChange)
-					this.props.onChange(newTags)
-			}
-		}
-	}
+  add = (event) => {
+    const { currentValue: val, tags } = this.state
+    const { onChange } = this.props
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      if (val && val.length > 0) {
+        const newTag = {
+          id: -(tags.length + 1),
+          name: val,
+        }
+        const newTags = tags.concat([newTag])
+        this.setState({
+          tags: newTags,
+          currentValue: '',
+        })
+        if (onChange) this.props.onChange(newTags)
+      }
+    }
+  }
 
-	change = ({target}) => {
-		this.setState({currentValue: target.value})
-		clearTimeout(this.timeout)
-		if (this.props.service && target.value) {
-			this.timeout = setTimeout(async () => {
-				this.setState({suggestPromise: this.props.service.find(target.value)})
-				const response = await this.state.suggestPromise
-				this.setState({suggestions: response.data})
-			}, 300)
-		} else if (!target.value) {
-			this.clear()
-		}
-	}
+  change = ({ target }) => {
+    const { service } = this.props
+    this.setState({ currentValue: target.value })
+    clearTimeout(this.timeout)
+    if (service && target.value) {
+      this.timeout = setTimeout(async () => {
+        this.setState({ suggestPromise: service.find(target.value) })
+        const response = await this.state.suggestPromise
+        this.setState({ suggestions: response.data })
+      }, 300)
+    } else if (!target.value) {
+      this.clear()
+    }
+  }
 
-	remove = (id) => {
-		const newTags = this.state.tags.filter(tag => tag.id !== id)
-		this.setState({tags: newTags})
-		if (this.props.onChange)
-			this.props.onChange(newTags)
-	}
+  remove = (id) => {
+    const { onChange } = this.props
+    const newTags = this.state.tags.filter((tag) => tag.id !== id)
+    this.setState({ tags: newTags })
+    if (onChange) onChange(newTags)
+  }
 
-	clear = () => this.setState({tags: [], currentValue: '', suggestions: []})
+  clear = () => this.setState({ tags: [], currentValue: '', suggestions: [] })
 
-	pickSuggest = (tag) => {
-		const newTags = this.state.tags.concat([tag])
-		this.setState({
-			tags: newTags,
-			suggestions: [],
-			currentValue: ''
-		})
-		if (this.props.onChange)
-			this.props.onChange(newTags)
-	}
+  pickSuggest = (tag) => {
+    const { onChange } = this.props
+    const newTags = this.state.tags.concat([tag])
+    this.setState({
+      tags: newTags,
+      suggestions: [],
+      currentValue: '',
+    })
+    if (onChange) onChange(newTags)
+  }
 
-	get tags() {
-		return this.state.tags.map((tag, index) => (
-			<span key={index} id={tag.id} className='tag'
-				onClick={() => {this.remove(tag.id)}}>
-				{tag.name}
-			</span>
-		))
-	}
+  get tags() {
+    return this.state.tags.map((tag, index) => (
+      <span
+        key={index}
+        id={tag.id}
+        className="tag"
+        onClick={() => this.remove(tag.id)}
+      >
+        {tag.name}
+      </span>
+    ))
+  }
 
-	get suggestions() {
-		return this.state.suggestions.map((tag, index) => (
-			<span key={index} className='suggest tag'
-				onClick={() => {this.pickSuggest(tag)}}
-			>
-				{tag.name}
-			</span>
-		))
-	}
+  get suggestions() {
+    return this.state.suggestions.map((tag, index) => (
+      <span
+        key={index}
+        className="suggest tag"
+        onClick={() => this.pickSuggest(tag)}
+      >
+        {tag.name}
+      </span>
+    ))
+  }
 
-	render() {
-		return (
-			<div className={`${this.props.className} ${this.classes.main}`}>
-				{this.tags}
-				<input type='text'
-					placeholder='find a tag'
-					value={this.state.currentValue}
-					onChange={this.change}
-					onKeyDown={this.add} />
-				{this.suggestions}
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className={`${this.props.className} ${this.classes.main}`}>
+        {this.tags}
+        <input
+          type="text"
+          placeholder="find a tag"
+          value={this.state.currentValue}
+          onChange={this.change}
+          onKeyDown={this.add}
+        />
+        {this.suggestions}
+      </div>
+    )
+  }
 }
+
+Tagger.propTypes = {
+  className: propTypes.string,
+  service: propTypes.object,
+  tags: propTypes.array,
+  onChange: propTypes.func,
+}
+
+export default Tagger
