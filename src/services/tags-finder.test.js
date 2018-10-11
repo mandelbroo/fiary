32961eff5d6b0f980b7b9aa580aeb688cea24087
Vitalii <vitalii.makarets@gmail.com>
2018-10-11 11:07:16 +0300

    tags-finder ignores uppercase

--- src/services/tags-finder.test.js
+++ src/services/tags-finder.test.js
@@ -1,17 +1,15 @@
 import { TagsFinder } from './tags-finder'
 
 describe('tags-finder', () => {
-	it('find method defined', () => {
-		const tagFinder = new TagsFinder()
-		expect(tagFinder.find).toBeDefined()
-	})
-	it('use provided http client', async () => {
-		const fakeAxios = {
-			get: jest.fn().mockReturnValue(Promise.resolve([]))
-		}
-		const tagFinder = new TagsFinder(fakeAxios)
-		const tags = await tagFinder.find('someTag')
-		expect(fakeAxios.get).toBeCalledWith('/tags?like=someTag')
-		expect(tags).toBeDefined()
-	})
+  it('find method defined', () => {
+    const tagFinder = new TagsFinder()
+    expect(tagFinder.find).toBeDefined()
+  })
+  it('use provided http client', async () => {
+    const faxios = { get: jest.fn().mockReturnValue(Promise.resolve([])) }
+    const tagFinder = new TagsFinder(faxios)
+    const tags = await tagFinder.find('UPPER-Cased')
+    expect(faxios.get).toBeCalledWith('/tags?like=upper-cased')
+    expect(tags).toBeDefined()
+  })
 })
