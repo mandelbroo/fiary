13a7519ef70219f5201b3360f8c3d01a8e279caa
Vitalii <vitalii.makarets@gmail.com>
2018-10-20 14:54:56 +0300

    fix tabs test

--- src/components/tab-bar/tab-bar.test.js
+++ src/components/tab-bar/tab-bar.test.js
@@ -5,16 +5,19 @@ import { mount } from 'enzyme'
 import TabBar, { PATHS } from './tab-bar'
 
 describe('TabBar', () => {
-	it('mount ok', () => {
-		const wrapper = mount(
-			<BrowserRouter>
-				<TabBar />
-			</BrowserRouter>)
-		const links = wrapper.find('a')
-		expect(links.length).toBe(PATHS.length)
-		expect(links.first().props().children).toBe(PATHS[0].name)
-		expect(links.first().props().href).toBe(`/${PATHS[0].path}`)
-		expect(links.last().props().children).toBe(PATHS[1].name)
-		expect(links.last().props().href).toBe(`/${PATHS[1].path}`)
-	})
+  it('mount ok', () => {
+    const wrapper = mount(
+      <BrowserRouter>
+        <TabBar />
+      </BrowserRouter>
+    )
+    const links = wrapper.find('a')
+    expect(links).toHaveLength(PATHS.length)
+    expect(links.at(0).props().children).toBe(PATHS[0].name)
+    expect(links.at(0).props().href).toBe(`/${PATHS[0].path}`)
+    expect(links.at(1).props().children).toBe(PATHS[1].name)
+    expect(links.at(1).props().href).toBe(`/${PATHS[1].path}`)
+    expect(links.at(2).props().children).toBe(PATHS[2].name)
+    expect(links.at(2).props().href).toBe(`/${PATHS[2].path}`)
+  })
 })
