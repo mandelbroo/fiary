dbc133fdd4def69a149d97607b840e815a5a6783
Vitalii <vitalii.makarets@gmail.com>
2018-10-20 21:10:37 +0300

    allow x-fram-options

--- server/routes.js
+++ server/routes.js
@@ -19,7 +19,10 @@ api.use(authorize)
 
 api.get('/entries', entriesController.getAll)
 api.get('/entries/:id(\\d+)/', entriesController.getById)
-api.get('/entries/:isoDate(\\d{4}-\\d{2}-\\d{2})/', entriesController.getIdByDate)
+api.get(
+  '/entries/:isoDate(\\d{4}-\\d{2}-\\d{2})/',
+  entriesController.getIdByDate
+)
 
 api.post('/records', recordsController.post)
 api.delete('/records/:id(\\d+)/', recordsController.destroy)
@@ -29,12 +32,13 @@ api.get('/users', usersController)
 api.get('/tags', tagsController.get)
 
 if (process.env.NODE_ENV === 'test') {
-	main.get('/with-error', (req, res) => {
-		req.undefinet.method
-	})
+  main.get('/with-error', (req, res) => {
+    req.undefinet.method
+  })
 }
 main.get('/*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '../build/index.html'))
+  res.header('X-Frame-Options', 'Allow')
+  res.sendFile(path.resolve(__dirname, '../build/index.html'))
 })
 
 const router = express.Router()
