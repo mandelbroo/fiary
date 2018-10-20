388f9d48e39c1c2b5167c14739ac0b9c2d019b6a
Vitalii <vitalii.makarets@gmail.com>
2018-10-20 23:44:02 +0300

    fix emojii warning

--- src/components/record-list/record-list.js
+++ src/components/record-list/record-list.js
@@ -6,39 +6,41 @@ import PropTypes from 'prop-types'
 import styles from './styles'
 
 export class RecordList extends React.Component {
-	classes = this.props.classes || {}
+  classes = this.props.classes || {}
 
-	NoRecords = (className) => (<span className={className}>no records yet</span>)
+  NoRecords = (className) => <span className={className}>no records yet</span>
 
-	CrossButton = (clickFunc) => {
-		return (<button
-			className={this.classes.removeButton}
-			onClick={clickFunc}
-			>❌</button>)
-	}
+  CrossButton = (clickFunc) => {
+    return (
+      <button className={this.classes.removeButton} onClick={clickFunc}>
+        <span role="img" aria-label="close">
+          ❌
+        </span>
+      </button>
+    )
+  }
 
-	removeButton = (item) =>
-		this.props.onRemove ? this.CrossButton(() => this.props.onRemove(item)) : ''
+  removeButton = (item) =>
+    this.props.onRemove ? this.CrossButton(() => this.props.onRemove(item)) : ''
 
-	render() {
-		const items = this.props.data ? [].concat(this.props.data) : []
-		const records = items.map((item, index) => (
-			<li key={index} className={this.classes.listItem}>
-				<RecordView data={item} />
-				{ this.removeButton(item) }
-			</li>)
-		)
-		const content = records.length > 0 ? records : this.NoRecords(this.classes.noRecords)
-		return (<ul className={this.classes.list}>
-				{ content }
-			</ul>)
-	}
+  render() {
+    const items = this.props.data ? [].concat(this.props.data) : []
+    const records = items.map((item, index) => (
+      <li key={index} className={this.classes.listItem}>
+        <RecordView data={item} />
+        {this.removeButton(item)}
+      </li>
+    ))
+    const content =
+      records.length > 0 ? records : this.NoRecords(this.classes.noRecords)
+    return <ul className={this.classes.list}>{content}</ul>
+  }
 }
 
 RecordList.propTypes = {
-	classes: PropTypes.object.isRequired,
-	data: PropTypes.array,
-	onRemove: PropTypes.func,
+  classes: PropTypes.object.isRequired,
+  data: PropTypes.array,
+  onRemove: PropTypes.func,
 }
 
 export default injectSheet(styles)(RecordList)
