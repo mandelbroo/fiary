3024e27b02b8cee8501275ec34f575a55c23dd63
Vitalii <vitalii.makarets@gmail.com>
2018-10-28 18:44:51 +0200

    prolong session time to 28 days

--- server/utils/jwt-generate.js
+++ server/utils/jwt-generate.js
@@ -1,19 +1,22 @@
 const jwt = require('jsonwebtoken')
 const JWT_WHITELIST = ['id', 'username', 'email', 'role']
+const EXPIRE_IN = 28 * 24 * 60 * 60
 
 function sanitize(obj, whitelist) {
-	return Object.keys(obj)
-			.filter(key => whitelist.includes(key))
-			.reduce((newObj, key) => {
-				newObj[key] = obj[key]
-				return newObj
-			}, {})
+  return Object.keys(obj)
+    .filter((key) => whitelist.includes(key))
+    .reduce((acc, key) => {
+      acc[key] = obj[key]
+      return acc
+    }, {})
 }
 
 module.exports = (user) => {
-	if (user) {
-		const userInfo = sanitize(user, JWT_WHITELIST)
-		const token = jwt.sign(userInfo, process.env.JWT_SECRET, {expiresIn: 86400})
-		return `Bearer ${token}`
-	}
+  if (user) {
+    const userInfo = sanitize(user, JWT_WHITELIST)
+    const token = jwt.sign(userInfo, process.env.JWT_SECRET, {
+      expiresIn: EXPIRE_IN,
+    })
+    return `Bearer ${token}`
+  }
 }
