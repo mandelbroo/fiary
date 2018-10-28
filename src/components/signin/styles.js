f07a67b5bdc7dc3ec9fc8d5f42b4e18a1d5cae28
Vitalii <vitalii.makarets@gmail.com>
2018-10-28 18:38:47 +0200

    bigger signin elements on mobile

--- src/components/signin/styles.js
+++ src/components/signin/styles.js
@@ -25,6 +25,9 @@ export default {
     '&:not(:last-of-type)': {
       marginBottom: '10px',
     },
+    [MOBILE]: {
+      fontSize: '20px',
+    },
   },
   button: {
     marginTop: '10px',
@@ -36,11 +39,15 @@ export default {
     width: '120px',
     padding: '10px',
     textTransform: 'uppercase',
-    fontWeithg: '700',
+    fontWeight: '700',
     letterSpacing: '0.3px',
     '&:focus': {
       border: 'solid 1px white',
       boxShadow: '0 4px 8px 0px rgba(0,0,0,0.3)',
+    },
+    [MOBILE]: {
+      fontSize: '20px',
+      padding: '10px 20px',
     },
   },
 }
