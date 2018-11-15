445caa753e9734e379de2d8d3cfdf7cde5356b05
Vitalii <vitalii.makarets@gmail.com>
2018-11-15 20:57:18 +0200

    update record action

--- src/models/record.js
+++ src/models/record.js
@@ -2,8 +2,12 @@ import Base from './base'
 import axios from 'axios'
 
 export default class Record extends Base {
-	static get endpoint() {return 'records'}
+  static get endpoint() {
+    return 'records'
+  }
 
-	static save = (data) => axios.post(Record.endpoint, data, Record.config)
-	static destroy = (id) => axios.delete(`${Record.endpoint}/${id}`, Record.config)
+  static create = (data) => axios.post(Record.endpoint, data, Record.config)
+  static update = (data) => axios.patch(Record.endpoint, data, Record.config)
+  static destroy = (id) =>
+    axios.delete(`${Record.endpoint}/${id}`, Record.config)
 }
