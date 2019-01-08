c336a870ed462230a79faa23ed1b7ef6f1f81633
Vitalii <vitalii.makarets@gmail.com>
2019-01-08 22:09:46 +0200

    fix cyrillic tags search

--- server/services/tags.js
+++ server/services/tags.js
@@ -2,24 +2,20 @@ const knex = require('../../db/connection')
 const Tag = require('../models/tag')
 
 module.exports = {
-	post: async (data) => {
-		return (
-			await knex('tags')
-				.insert(data)
-				.returning(['id', 'name'])
-		)
-		.map(tag => Tag.forge(tag))
-	},
-	get: async (fieldName, values) => {
-		return (
-			await knex('tags')
-				.whereIn(fieldName, values)
-				.returning(['id', 'name'])
-			)
-			.map(tag => Tag.forge(tag))
-	},
-	like: (value) => knex('tags')
-		.where('name', 'like', `${value}%`)
-		.select('id', 'name')
-		.limit(5)
+  post: async (data) => {
+    return (await knex('tags')
+      .insert(data)
+      .returning(['id', 'name'])).map((tag) => Tag.forge(tag))
+  },
+  get: async (fieldName, values) => {
+    return (await knex('tags')
+      .whereIn(fieldName, values)
+      .returning(['id', 'name'])).map((tag) => Tag.forge(tag))
+  },
+  like: (value) => {
+    return knex('tags')
+      .whereRaw(`lower(name) like ?`, `${value}%`)
+      .select('id', 'name')
+      .limit(5)
+  },
 }
